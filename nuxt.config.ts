d1b85ff2ed0e92085693fc856e8b2ef2d4bdb099
Kevin Monestel Amador <39996057+KevinMonestel@users.noreply.github.com>
2022-09-14 16:28:49 -0600

    Adding color theme.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase', '@formkit/nuxt', 'nuxt-icon'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase', '@formkit/nuxt', 'nuxt-icon', '@nuxtjs/color-mode'],
   formkit: {
     configFile: './formkit.config.ts'
+  },
+  colorMode: {
+    classSuffix: ''
   }
 })
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,17 @@ import { Config } from 'tailwindcss'
 const formKitTailwind = require('@formkit/themes/tailwindcss');
 
 export default <Config> {
+  theme: {
+    darkSelector: '.dark-mode'
+  },
+  variants: {
+    backgroundColor: ["dark", "dark-hover", "dark-group-hover", "dark-even", "dark-odd", "hover", "responsive"],
+    borderColor: ["dark", "dark-focus", "dark-focus-within", "hover", "responsive"],
+    textColor: ["dark", "dark-hover", "dark-active", "hover", "responsive"]
+  },
   content: ['./formkit.config.ts'],
   plugins: [
-    formKitTailwind
-  ]
+    formKitTailwind,
+  ],
+  darkMode: 'class'
 }
