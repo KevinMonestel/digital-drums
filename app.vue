7345e420516c94ef55670fae60deffb98fafe25c
Kevin Monestel Amador <39996057+KevinMonestel@users.noreply.github.com>
2022-09-14 16:16:41 -0600

    Installing nuxt-icon

--- app.vue
+++ app.vue
@@ -1,27 +1,5 @@
 <template>
   <div>
-    <FormKit
-    v-for="type in inputTypes"
-    :key="type"
-    :label="`This is a ${type} input`"
-    :type="type"
-    :placeholder="`${type} input placeholder`"
-    :options="inputOptions[type] ? inputOptions[type] : null"
-    :help="`Help text for the ${type} input`"
-    :multiple="type === 'file' ? true : undefined"
-    :disabled="type === 'date' ? true : undefined"
-    :validation="type === 'email' ? 'required|email' : 'required'"
-    :validation-visibility="type === 'email' ? 'live' : 'blur'"
-    :validation-label="type"
-  />
+    <Icon name="uil:github" />
   </div>
 </template>
-
-<script setup>
-const inputTypes = ['button', 'checkbox', 'color', 'date', 'datetime-local', 'email', 'file', 'month', 'number', 'password', 'radio', 'range', 'search', 'select', 'submit', 'tel', 'text', 'textarea', 'time', 'url', 'week']
-const inputOptions = {
-  checkbox: ['I like Tailwind', 'I also like FormKit', 'I like other things too'],
-  radio: ['I like Tailwind', 'I like FormKit', 'I like everything'],
-  select: ['I like Tailwind', 'I like FormKit', 'I like everything'],
-}
-</script>
