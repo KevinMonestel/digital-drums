c86c9d435f680690a977c3ffc4a1e858cb1d43d4
Kevin Monestel Amador <39996057+KevinMonestel@users.noreply.github.com>
2022-09-15 13:28:32 -0600

    Delete vue-final-modal.js

--- plugins/vue-final-modal.js
+++
@@ -1,9 +0,0 @@
-import { vfmPlugin }from 'vue-final-modal'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(vfmPlugin({
-    key: '$vfm',
-    componentName: 'VueFinalModal',
-    dynamicContainerName: 'ModalsContainer'
-  }))
-})
